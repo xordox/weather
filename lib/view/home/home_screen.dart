b97266e5e48e60fe55c3f9fe9eb237140b44799a
PrabinMaharjan <prabin.muncha@gmail.com>
2022-12-11 08:23:52 +0545

    updated debounce feature in searching

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
@@ -17,16 +18,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  
+  Timer? _debounce;
+
   String date = DateFormat("yMMMMd").format(DateTime.now());
 
   final GlobalController globalController = Get.find<GlobalController>();
-  final TextEditingController _controller = TextEditingController(text:AppSharedPreferences.getCityInfo());
+  final TextEditingController _controller =
+      TextEditingController(text: AppSharedPreferences.getCityInfo());
+
+  _onSearchChanged() async {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(seconds: 2), () async {
+      AppSharedPreferences().saveCity(_controller.text);
+      await globalController.fetchCityWeather(_controller.text);
+    });
+  }
 
   @override
   void initState() {
     getAddress(globalController.getLattitude().value,
         globalController.getLongitude().value);
+    _controller.addListener(_onSearchChanged);
     super.initState();
   }
 
@@ -39,57 +51,64 @@ class _HomeScreenState extends State<HomeScreen> {
     });
     log("${placemark[0]}");
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          TextButton(onPressed: ()=>Get.toNamed(SplashScreen.route), child: const Text("Back",style: TextStyle(color: Colors.white),),),
+          TextButton(
+            onPressed: () => Get.toNamed(SplashScreen.route),
+            child: const Text(
+              "Back",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
         ],
       ),
       body: SafeArea(
-        child: Obx(() =>
-         globalController.checkLoading().isTrue
+        child: Obx(() => globalController.checkLoading().isTrue
             ? const Center(
                 child: CircularProgressIndicator(),
-              ):
-             ListView(
+              )
+            : ListView(
                 scrollDirection: Axis.vertical,
                 children: [
                   const SizedBox(
                     height: 20,
                   ),
-                 Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20),
-          alignment: Alignment.topLeft,
-          child: Text(
-            globalController.getWeather.value.location!.name,
-            style: const TextStyle(fontSize: 35),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20),
-          alignment: Alignment.topLeft,
-          child: Text(
-            date,
-            style: const TextStyle(fontSize: 14, color: Colors.grey),
-          ),
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20),
-          alignment: Alignment.topLeft,
-          child: Text(
-            "Temp: ${globalController.getWeather.value.current?.tempC} °C",
-            style: const TextStyle(fontSize: 14, color: Colors.grey),
-          ),
-        ),Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20),
-          alignment: Alignment.topLeft,
-          child: Text(
-            "Condition: ${globalController.getWeather.value.current?.condition.text}",
-            style: const TextStyle(fontSize: 14, color: Colors.grey),
-          ),
-        ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      globalController.getWeather.value.location!.name,
+                      style: const TextStyle(fontSize: 35),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      date,
+                      style: const TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Temp: ${globalController.getWeather.value.current?.tempC} °C",
+                      style: const TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 20),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Condition: ${globalController.getWeather.value.current?.condition.text}",
+                      style: const TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
@@ -97,24 +116,35 @@ class _HomeScreenState extends State<HomeScreen> {
                         width: 200,
                         child: TextFormField(
                           decoration: const InputDecoration(
-                        hintText :"Enter location",
-                      ),
-                          controller: _controller,                          
+                            hintText: "Enter location",
+                          ),
+                          controller: _controller,
                         ),
                       ),
-                      ElevatedButton(onPressed: ()async{
-                        if(_controller.text.isEmpty){
-                          Get.snackbar("Location is empty", "");
-                        }
-                        else{AppSharedPreferences().saveCity(_controller.text);
-                        await globalController.fetchCityWeather(_controller.text);}
-                        },
-                         child: _controller.text.isEmpty? const Text("Save"): const Text("Update"))
+                      ElevatedButton(
+                          onPressed: () async {
+                            if (_controller.text.isEmpty) {
+                              Get.snackbar("Location is empty", "");
+                            } else {
+                              AppSharedPreferences().saveCity(_controller.text);
+                              await globalController
+                                  .fetchCityWeather(_controller.text);
+                            }
+                          },
+                          child: _controller.text.isEmpty
+                              ? const Text("Save")
+                              : const Text("Update"))
                     ],
                   )
                 ],
               )),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    super.dispose();
   }
 }
